b562d672f1144a3e4faa8588d0222ab99d7274db
Kenneth Postigo <7651554+kpostigo@users.noreply.github.com>
2021-02-05 14:35:13 -0500

    added about page, cleaned up navbar, added container for content block

--- server.py
+++ server.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 def home():
 	return render_template("index.html")
 
-
+@app.route('/about')
+def about():
+	return render_template("about.html")
 
 
 @app.route('/results', methods=['POST', 'GET'])
