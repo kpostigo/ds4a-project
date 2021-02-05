50f38fa1d6d714403e99cd7a353b7f2ed13756b1
Lianna Patterson Ware <liannapattersonware@Liannas-MacBook-Pro.local>
2021-02-04 19:56:51 -0500

    cleaning up inputs file

--- scripts/inputs.py
+++ scripts/inputs.py
@@ -8,17 +8,15 @@ school_name = 'Brown University'
 tuition_weight = 9
 graduation_weight = 8
 employment_weight = 3
-urban_weight = 10
 
-total_weight = tuition_weight+graduation_weight+employment_weight+urban_weight
+total_weight = tuition_weight+graduation_weight+employment_weight
 
 # not sure about the syntax for the score column but wanted to include the code
 df['Score'] = (
     (
-      ((tuition_weight)*(sample_run['Rank_Tuition']))
-    + ((graduation_weight)*(sample_run['Rank_Grad_Rate']))
-    + ((employment_weight)*(sample_run['Rank_Employment_Rate']))
-    + ((urban_weight)*(sample_run['Rank_Urban']))
+      ((tuition_weight)*(df['Rank_Tuition']))
+    + ((graduation_weight)*(df'Rank_Grad_Rate']))
+    + ((employment_weight)*(df['Rank_Employment_Rate']))
     )/total_weight)
 
 # grand rank column will provide definitive ranking based on weights
